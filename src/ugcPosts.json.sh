626ae2b5c8fcb499f7935838d0f8f0ad720dcedd
Tarsis <br.tmvdl@gmail.com>
2023-12-15 13:28:37 -0300

    Updating app files

--- src/ugcPosts.json.sh
+++ src/ugcPosts.json.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/sh
 
-jq -n --arg author "urn:li:person:${1}" --arg text "${2}" '{"author": $author,"lifecycleState": "PUBLISHED","specificContent": {"com.linkedin.ugc.ShareContent": {"shareCommentary": {"text": $text},"shareMediaCategory": "NONE"}},"visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"}}'
+text="${2}"
+
+author="urn:li:person:${1}"
+
+lifecycleState="PUBLISHED"
+
+specificContent=""
+
+specificContent=$( jq -n --arg text "${text}" '{"com.linkedin.ugc.ShareContent": {"shareCommentary": {"text": $text},"shareMediaCategory": "NONE"}}' )
+
+visibility='{"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"}'
+
+jq -n --arg author "${author}" --arg lifecycleState "${lifecycleState}" --argjson specificContent "${specificContent}" --argjson visibility "${visibility}" '{"author": $author,"lifecycleState": $lifecycleState,"specificContent": $specificContent,"visibility": $visibility}'
